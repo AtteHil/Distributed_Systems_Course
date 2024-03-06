dda9b844cbcb2a26cb24e819b9fe819480259e1f
Atte <attehiltunen@hotmail.com>
2024-03-06 14:59:16 +0200

    XMLRPC correct implementation

--- assignment2/client/main.py
+++ assignment2/client/main.py
@@ -1,36 +1,51 @@
-import socket
-#Server information
-HOST="127.0.0.1"
-PORT = 3570
-FORMAT = 'UTF-8'
-DISCONNECT_MESSAGE = "!Quit"
-HEADER = 64
+import xmlrpc.client
+class NotebookClient:
+    def __init__(self):
+        self.server = xmlrpc.client.ServerProxy('http://localhost:8000')
 
-ADDR = (HOST, PORT)
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#connect to server
-client.connect(ADDR)
+    def add_note(self):
+        topic = input("Enter the topic of the note: ")
+        name = input("Enter the name of the note: ")
+        text = input("Enter the text of the note: ")
+        response = self.server.addNote(topic, name, text)
+        if(response):
+            print("Note added succesfully!")
 
-
-
- 
-def main(msg): ## sneds msg to server
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADER - len(send_length))
-    client.send(send_length)
-    client.send(message)
-    print(client.recv(2048).decode(FORMAT))
-
-    
-
-
-    
-main("Hello World")
-input()
-main("Hello Matt")
-input()
-main("Hello Everyone")
-input()
-main(DISCONNECT_MESSAGE)
+    def get_notes_by_topic(self):
+        topic = input("Enter the topic to retrieve notes: ")
+        notes = self.server.getNotesByTopic(topic)
+        if notes:
+            print(f"\nNotes for topic '{topic}':")
+            for note in notes:
+                print(f"Name: {note['note']}")
+                print(f"Timestamp: {note['timestamp']}")
+                print(f"Text: {note['text']}\n")
+        else:
+            print(f"No notes found for topic '{topic}'")
+    def findFromWikipedia(self):
+        searchTerm= input("Enter the search word from wikipedia: ")
+        topic= input("Enter the topic you want this information to be added: ")
+        information = self.server.findFromWikiApi(searchTerm, topic)
+        if information:
+            print("Data found and added to topic.")
+        
+if __name__ == '__main__':
+    client = NotebookClient()
+    while True:
+        print("\nMenu: ")
+        print("1. Add a note")
+        print("2. Get notes")
+        print("3. Find from wikipedia and and add to topic")
+        print("4. Exit")
+        selection = input("What you want to do: ")
+        match selection:
+            case "1":
+                client.add_note()
+            case "2":
+                client.get_notes_by_topic()
+            case "3":
+                client.findFromWikipedia()
+            case "4":
+                break
+            case _:
+                print("Invalid selection. Try again!")
--- assignment2/server/server.py
+++ assignment2/server/server.py
@@ -1,41 +1,82 @@
-import socket
-import threading
-HEADER = 64
-HOST = "127.0.0.1" #Localhost
-PORT = 3570
-DISCONNECT_MESSAGE = "!Quit"
-ADDR = (HOST, PORT)
-FORMAT = 'UTF-8'
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(ADDR)
+# server.py
+from xmlrpc.server import SimpleXMLRPCServer
+import datetime
+import xml.etree.ElementTree as ET
+import requests
+## help found here https://www.tutorialspoint.com/create-xml-documents-using-python
+## and here https://docs.python.org/3/library/xml.etree.elementtree.html
+class NoteBook:
+    def __init__(self):
+        self.database = "notebook.xml"
+        self.tree = ET.parse(self.database)
+        self.root = self.tree.getroot()
+        self.URL = "https://en.wikipedia.org/w/api.php"
+        self.S = requests.Session()
+    def printOut(self, topic):
+        return topic
+    
+    def save_to_xml(self):
+        self.tree.write(self.database)
+    
+    def addNote(self, topic,name, text):
+        child =self.findTopic(topic)
+        if len(child)!=0:
+            self.makeNote(child, name, text, 0)
+            return True
+        topic_elem = ET.Element('topic') ## make note xml structure
+        topic_elem.set('name', topic)
+        self.makeNote(topic_elem, name, text, 1)
+        return True
+    
+    def getNotesByTopic(self, topic):
+        foundNotes = []
+        child =self.findTopic(topic)
+        if len(child)!=0:
+            for note in child.findall('note'):
+                foundNote = {
+                    'note': note.get('name'),
+                    'text': note.find('text').text,
+                    'timestamp': note.find('timestamp').text
+                }
+                foundNotes.append(foundNote)
+        return foundNotes
+    
+    def findFromWikiApi(self, searchTerm, topic):
+        PARAMS = { ## Cannot get the descriptions because this is said on the site "On Wikimedia wikis descriptions are disabled due to performance reasons, so the second array only contains empty strings"
+            ##https://www.mediawiki.org/wiki/API:Opensearch
+            "action": "opensearch",
+            "namespace": "0",
+            "search": searchTerm,
+            "limit": "3",
+            "format": "json"
+        }
+        R = self.S.get(url=self.URL, params=PARAMS)
+        if(R):
+            DATA = R.json()
+            self.addNote(topic, searchTerm, DATA[3][0])
+            
+        
+    
+    def makeNote(self,topic_elem, name, text, new):
+        note_elem = ET.SubElement(topic_elem, 'note')
+        note_elem.set('name',name) ## add topic
+        text_elem = ET.SubElement(note_elem, 'text')
+        text_elem.text = text ## add text to note
+        timestamp_elem = ET.SubElement(note_elem, 'timestamp')
+        timestamp_elem.text = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') ##add timestamp
+        if new:
+            self.root.append(topic_elem)
+        self.save_to_xml()
 
+    def findTopic(self,name):
+        for child in self.root.findall('topic'):
+            topic_name = child.get('name')
+            if topic_name == name:
+                return child
+        return ""
 
-
-def handle_input(conn,addr):
-    print(f"[CONNECTED] {addr} connected")
-    connected = True
-    while connected:
-        msg_length = conn.recv(HEADER).decode(FORMAT)
-        if msg_length:
-            msg_length = int(msg_length)
-            # Receive the full message
-            msg = conn.recv(msg_length).decode(FORMAT)
-            if msg == DISCONNECT_MESSAGE:
-                
-                connected = False
-            print(f"[{addr}] {msg}") # print out the message
-            conn.send("Msg received".encode(FORMAT))
-    print(f"[DISCONNECT] {addr} disconnected")
-    conn.close()
-
-def main():
-    server.listen()
-    print(f"[LISTENING] Server listening on {HOST}")
-    while True:
-        conn, addr = server.accept()
-        thread = threading.Thread(target=handle_input, args=(conn, addr)) ## make new thread for incoming connection. 
-        thread.start()
-        print(f"[ACTIVE] {threading.active_count()-1}")
-
-
-main()
+if __name__ == '__main__':
+    server = SimpleXMLRPCServer(('localhost', 8000), allow_none=True)
+    server.register_instance(NoteBook())
+    print("Server started on localhost:8000...")
+    server.serve_forever()
